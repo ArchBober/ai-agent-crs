bb888e4515e78e9a38fa7a4f299ca3d46f7fb5e8
archbober <git@archbober.com>
2025-10-06 10:45:05 +0200

    getfilecontect

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 from functions.get_files_info import get_files_info
-
+from functions.get_file_content import get_file_content
 
 def test():
     result = get_files_info("calculator", ".")
@@ -17,6 +17,15 @@ def test():
 
     result = get_files_info("calculator", "../")
     print("Result for '../' directory:")
+    print(result)
+
+    result = get_file_content("calculator", "main.py")
+    print(result)
+
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print(result)
+
+    result = get_file_content("calculator", "/bin/cat")
     print(result)
 
 
