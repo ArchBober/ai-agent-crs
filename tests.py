3f6597f8bf045b8a942f3561cc00e408e31243d3
archbober <git@archbober.com>
2025-10-07 01:54:25 +0200

    run_python

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 def test():
     result = get_files_info("calculator", ".")
@@ -36,6 +37,21 @@ def test():
     print(result)
 
     result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print(result)
+
+    result = run_python_file("calculator", "main.py")
+    print(result)
+
+    result = run_python_file("calculator", "main.py", ["3 + 5"])
+    print(result)
+
+    result = run_python_file("calculator", "tests.py")
+    print(result)
+
+    result = run_python_file("calculator", "../main.py")
+    print(result)
+
+    result = run_python_file("calculator", "nonexistent.py")
     print(result)
 
 
