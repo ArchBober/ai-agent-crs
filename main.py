a9b1e850c3296f00bf73042129c9e24f4f04fd4d
archbober <git@archbober.com>
2025-10-06 09:12:49 +0200

    add cli as prompt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 
 load_dotenv()
@@ -8,12 +9,20 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
+    if len(sys.argv) <= 1:
+        sys.exit(1)
+    elif len(sys.argv[1]) < 1:
+        sys.exit(1) 
+
     print("Hello from ai-agent-crs!")
+
+    client_prompt = sys.argv[1]
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=client_prompt
     )
-
+    
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
